6eedb6b4f9d4e9c0698b9e162c3284b05cd79168
luisorb <luiso.rb94@gmail.com>
2026-06-06 01:08:43 -0400

    feat(Gupo Digitador): Verificar y validar grupo Digitador

--- users/apps.py
+++ users/apps.py
@@ -1,4 +1,11 @@
 from django.apps import AppConfig
+from django.db import OperationalError, ProgrammingError
+
+
+def _create_digitador_group():
+    from django.contrib.auth.models import Group
+
+    Group.objects.get_or_create(name="Digitador")
 
 
 class UsersConfig(AppConfig):
@@ -6,4 +13,7 @@ class UsersConfig(AppConfig):
     name = "users"
 
     def ready(self):
-        from . import signals  # noqa: F401
+        try:
+            _create_digitador_group()
+        except (OperationalError, ProgrammingError):
+            pass  # BD aún no lista (primera migración)
